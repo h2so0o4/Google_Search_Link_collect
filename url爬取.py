be0dc2135a4a61e01eae3b07206343ff3bca6ba0
H2s04 <5643934+H2s04@user.noreply.gitee.com>
2021-03-11 17:53:49 +0800

    忽略https证书告警

--- url爬取.py
+++ url爬取.py
@@ -2,6 +2,9 @@ import re
 import urllib
 import requests
 from bs4 import BeautifulSoup
+import urllib3
+
+urllib3.disable_warnings() #忽略https证书告警
 
 # desktop user-agent
 USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:65.0) Gecko/20100101 Firefox/65.0"
@@ -9,7 +12,7 @@ USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:65.0) Gecko/20100
 MOBILE_USER_AGENT = "Mozilla/5.0 (Linux; Android 7.0; SM-G930V Build/NRD90M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.125 Mobile Safari/537.36"
 
 # query为搜索内容
-query = "洛阳inurl:php?id=3 and site:.com"
+query = "影视inurl:php?id=3 and site:.com"
 # num为一次搜索的数量
 num = 50
 query = query.replace(' ', '+')
