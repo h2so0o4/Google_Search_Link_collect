057806cfb13f63652578652dbf91f44568deb510
H2s04 <5643934+H2s04@user.noreply.gitee.com>
2021-03-11 17:36:16 +0800

    增加可以设置URL数量

--- url爬取.py
+++ url爬取.py
@@ -9,9 +9,11 @@ USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:65.0) Gecko/20100
 MOBILE_USER_AGENT = "Mozilla/5.0 (Linux; Android 7.0; SM-G930V Build/NRD90M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.125 Mobile Safari/537.36"
 
 # query为搜索内容
-query = "inurl:.php?id= and site:.cn"
+query = "洛阳inurl:php?id=3 and site:.com"
+# num为一次搜索的数量
+num = 50
 query = query.replace(' ', '+')
-URL = f"https://google.com/search?q={query}"
+URL = f"https://google.com/search?q={query}&num={num}"
 
 headers = {"user-agent": USER_AGENT}
 resp = requests.get(URL, headers=headers, verify=False)
@@ -29,8 +31,13 @@ if resp.status_code == 200:
                 try:
                     link = anchors[i].attrs['href']  # 提取字典内容
 
-                    # 正则过滤URL，删除掉一些没用的url
+                    # 正则过滤URL，删除掉一些乱码
                     if re.match('/', link) == None and re.match('(.*)google.com', link) == None and link != '#':
+
+                        # 过滤掉重复的URL
+                        for i in results:
+                            if i.split(".com")[0] == link.split(".com")[0] :
+                                link = ""
                         results.append(link)
                 except:
                     pass
@@ -42,3 +49,5 @@ if resp.status_code == 200:
     with open("urls.txt", "w") as f:
         for i in results:
             f.write(i + "\n")
+
+
